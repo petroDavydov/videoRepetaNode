60bc8591a42a417adca2ae7d6685808216e3b670
petroDavydov <petrodavydov@gmail.com>
2021-07-16 20:31:00 +0300

    add

---
+++ src/index.js
@@ -0,0 +1,26 @@
+// import validatePassword from "./js/validate-password";
+import {addUser} from './js/api-service';
+
+// addUser('mango')
+// addUser('Bill')
+// addUser('Rock')
+addUser(1)
+// console.log(validatePassword('sdfhs4$'));
+// import {
+//   fetchAllUsers,
+//   fetchUserById,
+//   updateUserById,
+//   x as value,
+//   y as name,
+// } from "./js/api-service";
+
+// console.log(validatePassword);
+// console.log(`=============================`);
+// console.log(fetchAllUsers);
+// console.log(fetchUserById);
+// console.log(updateUserById);
+// console.log(value);
+// console.log(name);
+
+// import * as apiService from "./js/api-service";
+// console.log(apiService);
